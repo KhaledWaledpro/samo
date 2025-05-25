9cf8bb2d84137524af9e615f54bac0351e77ffd9
KhaledWaledpro <khalidwalidabdalnaseer22@gmail.com>
2025-05-25 07:59:48 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
 return array(
-    'sitename' => 'PHPSocial',
+    'sitename' => 'samol',
     'theme' => 'light.css', // Themes are stored in the /css directory, remeber to include the extension when changing the theme
     'adminEnabled' => true,
     // Use [sitename] as a placeholder for the website name, it will be automaticly replaced.
-    'registerPrompts' => array("Join in on the fun of [sitename],", "Join in on the conversation,", "Talk to your friends on [sitename],")
+    'registerPrompts' => array("Join in on the fun of [samol],", "Join in on the conversation,", "Talk to your friends on [samol],")
 );
